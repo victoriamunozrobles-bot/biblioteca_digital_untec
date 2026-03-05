479f2073ec39c3fab99c5b9efc696080befba4ca
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-03-05 11:49:18 -0300

    Add: implementa la clase Socio para el manejo de usuarios

---
+++ src/main/java/cl/biblioteca/digital/model/Socio.java
@@ -0,0 +1,43 @@
+package cl.biblioteca.digital.model;
+
+public class Socio {
+    private int idSocio;
+    private String nombre;
+    private String email;
+    private String password;
+
+    public Socio() {
+    }
+
+    public int getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(int idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

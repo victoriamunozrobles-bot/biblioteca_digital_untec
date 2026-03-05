be92a85d26fe549129c774682d1eed81dd1d8a31
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-03-05 11:46:26 -0300

    Refactor: mejora del formato de la clase Prestamo con sus métodos

--- src/main/java/cl/biblioteca/digital/model/Prestamo.java
+++ src/main/java/cl/biblioteca/digital/model/Prestamo.java
@@ -11,22 +11,62 @@ public class Prestamo {
     private String nombreSocio;
     private String tituloLibro;
 
-    public Prestamo() {}
+    public Prestamo() {
+    }
 
-    public int getIdPrestamo() { return idPrestamo; }
-    public void setIdPrestamo(int idPrestamo) { this.idPrestamo = idPrestamo; }
-    public int getIdSocio() { return idSocio; }
-    public void setIdSocio(int idSocio) { this.idSocio = idSocio; }
-    public int getIdLibro() { return idLibro; }
-    public void setIdLibro(int idLibro) { this.idLibro = idLibro; }
-    public Date getFechaPrestamo() { return fechaPrestamo; }
-    public void setFechaPrestamo(Date fechaPrestamo) { this.fechaPrestamo = fechaPrestamo; }
-    public Date getFechaDevolucion() { return fechaDevolucion; }
-    public void setFechaDevolucion(Date fechaDevolucion) { this.fechaDevolucion = fechaDevolucion; }
-    public String getNombreSocio() { return nombreSocio; }
-    public void setNombreSocio(String nombreSocio) { this.nombreSocio = nombreSocio; }
-    public String getTituloLibro() { return tituloLibro; }
-    public void setTituloLibro(String tituloLibro) { this.tituloLibro = tituloLibro; }
-}{
+    public int getIdPrestamo() {
+        return idPrestamo;
+    }
 
+    public void setIdPrestamo(int idPrestamo) {
+        this.idPrestamo = idPrestamo;
+    }
+
+    public int getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(int idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public int getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(Date fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String getNombreSocio() {
+        return nombreSocio;
+    }
+
+    public void setNombreSocio(String nombreSocio) {
+        this.nombreSocio = nombreSocio;
+    }
+
+    public String getTituloLibro() {
+        return tituloLibro;
+    }
+
+    public void setTituloLibro(String tituloLibro) {
+        this.tituloLibro = tituloLibro;
+    }
 }

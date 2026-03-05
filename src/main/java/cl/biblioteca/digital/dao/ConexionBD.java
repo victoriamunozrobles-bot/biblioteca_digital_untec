0ad9b9113168fcb78d7b4d6319c6e67abd762984
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-03-05 11:18:32 -0300

    Refactor: implementación del manejo de conexión en la clase ConexionDB

--- src/main/java/cl/biblioteca/digital/dao/ConexionBD.java
+++ src/main/java/cl/biblioteca/digital/dao/ConexionBD.java
@@ -1,5 +1,37 @@
 package cl.biblioteca.digital.dao;
 
-public class ConexionBD {
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class ConexionBD {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = ConexionBD.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                System.err.println("Error: No se encontró el archivo db.properties");
+            } else {
+                props.load(input);
+                Class.forName("org.mariadb.jdbc.Driver");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection obtenerConexion() {
+        try {
+            return DriverManager.getConnection(
+                    props.getProperty("db.url"),
+                    props.getProperty("db.user"),
+                    props.getProperty("db.password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
